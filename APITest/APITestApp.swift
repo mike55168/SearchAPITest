ff5a77689338451c96886de6c661f7aca5f335b3
hookyang <mike55168@gmail.com>
2021-06-20 10:51:58 +0800

    Initial Commit

---
+++ APITest/APITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APITestApp.swift
+//  APITest
+//
+//  Created by HookYang on 2021/6/20.
+//
+
+import SwiftUI
+
+@main
+struct APITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
